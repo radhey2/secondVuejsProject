8fbefe481b491264b02b3fe58ae175a2572ccfa4
lenovo <radheshyamb9@gmail.com>
2022-06-07 22:37:03 +0530

    ADDED : Use For Each loop in V-Modal

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  <span v-for="option in options" :key="option">
+    <input v-model="checkedNames" :id="option.value" :value="option.value" type="checkbox">
+    <label for="option.value">  {{option.value}}</label>
+  </span>
+
+  <br>
+  <span>Checked names: {{ checkedNames }}</span>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      checkedNames: [],
+      options: [
+        { value: 'John' },
+        { value: 'Paul' },
+        { value: 'George' },
+        { value: 'Ringo' }
+      ]
+    }
+  }
+}
+</script>
